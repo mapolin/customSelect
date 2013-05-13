fdc6d39ea77921d46c91a41c5d910b54b0b86f69
Martin Atzinov <mapanogg@gmail.com>
2013-05-13 16:00:29 +0300

    Added filtering functionality
    Works only if jScrollPane is present and given as an argument

--- jquery.custom.select.js
+++ jquery.custom.select.js
@@ -1,6 +1,8 @@
-function CustomSelect(custom, original) {
+function CustomSelect(custom, original, autoSearch, scrollPane) {
     if(!custom || !original) return false;
 
+    this.autoSearch = ( autoSearch != null) ? false : autoSearch;
+    this.scrollPane = ( this.autoSearch ) ? scrollPane.data('jsp') : false;
     this.container = custom;
     this.originalSelect = original;
     this.defaultVisible = custom.find('.default');
@@ -23,6 +25,18 @@ CustomSelect.prototype.toggleMenu = function() {
         this.openMenu();
     }
 };
+CustomSelect.prototype.findItem = function(value) {
+    if( !this.scrollPane ) return;
+
+    for(var i = 0; i < this.items.length; i++) {
+        if(this.items.eq(i).html().toLowerCase().match(value)) {
+            this.items.eq(i).addClass('hover');
+
+            this.scrollPane.scrollTo( parseInt(this.items.eq(i).position().top) );
+            return;
+        }
+    }
+};
 CustomSelect.prototype.findIndex = function(clicked) {
     var ind = 0;
     this.items.each(function(index) {
@@ -32,6 +46,10 @@ CustomSelect.prototype.findIndex = function(clicked) {
     });
 
     return ind;
+};
+CustomSelect.prototype.handleEnter = function() {
+    var target = this.items.filter('.hover');
+    this.handleClick(target);
 };
 CustomSelect.prototype.handleClick = function(target) {
     var val, text;
@@ -61,6 +79,22 @@ CustomSelect.prototype.init = function() {
         _this.toggleMenu();
     });
 
+    if( this.autoSearch ) {
+        this.defaultVisible.bind({
+            keyup: function(evt) {
+                _this.findItem($(this).val());
+            },
+            keydown: function(evt) {
+                if( evt.keyCode == '13' ) {
+                    _this.handleEnter();
+                }
+                else {
+                    _this.findItem($(this).val());
+                }
+            }
+        });
+    }
+
     $(document).bind('click', function(evt) {
         evt.stopPropagation();
 
@@ -69,10 +103,10 @@ CustomSelect.prototype.init = function() {
         }
     });
 };
-$.fn.customSelect = function(original) {
+$.fn.customSelect = function(original, autoSearch, scrollPane) {
     return this.each(function() {
         var orig = $(this).find(original);
-        var cSelect = new CustomSelect($(this), orig);
+        var cSelect = new CustomSelect($(this), orig, autoSearch);
         $(this).data('customSelect', cSelect);
     });
 };
